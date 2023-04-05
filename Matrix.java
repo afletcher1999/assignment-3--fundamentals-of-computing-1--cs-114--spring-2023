ad9fcd78bf5065bef057189f92d6427ae200530b
afletcher1999 <123336769+afletcher1999@users.noreply.github.com>
2023-04-05 15:01:42 -0400

    Initial Commit

--- Matrix.java
+++ Matrix.java
@@ -1,2 +1,50 @@
+import java.awt.Color;
 public class Matrix {
+  private int[][] matrix;
+  private int size;
+
+  public Matrix(int size) {
+    this.size = size;
+    matrix = new int[size][size];
+    System.out.printf("Created %dx%d matrix.\n", size, size);
+  }
+
+  private void swap(int x1, int y1, int x2, int y2) {
+    if (x1 + y1 == size - 1 || x2 + y2 == size - 1) {
+      return; // do not swap elements on the diagonal
+    }
+    int temp = matrix[x1][y1];
+    matrix[x1][y1] = matrix[x2][y2];
+    matrix[x2][y2] = temp;
+  }
+
+  public void printMatrix() {
+    for (int row = 0; row < size; row++) {
+      for (int col = 0; col < size; col++) {
+        if (row + col == size - 1) {
+          System.out.print("\u001B[33m"); // set background color to yellow using ANSI escape code
+        }
+        System.out.printf("%-4d", matrix[row][col]);
+        System.out.print("\u001B[0m"); // reset background color
+      }
+    System.out.println();
+    }
+  }
+
+  public void populateMatrix() {
+    int counter = 1;
+    for (int row = 0; row < size; row++) {
+      for (int col = 0; col < size; col++) {
+          matrix[row][col] = counter++;
+      }
+    }
+  }
+
+  public void flipMatrix() {
+    for (int row = 0; row < size; row++) {
+      for (int col = 0; col < size - row - 1; col++) {
+        swap(row, col, size - col - 1, size - row - 1);
+      }
+    }
+  }
 }
--- RunMatrix.java
+++ RunMatrix.java
@@ -2,5 +2,26 @@ import java.util.Scanner;
 
 public class RunMatrix {
   public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.print("Enter matrix size: ");
+    int size = scanner.nextInt();
+
+     // Step 2: Create a matrix of 0's and print it
+    Matrix matrix = new Matrix(size);
+    System.out.println("Matrix of 0's:");
+    matrix.printMatrix();
+
+
+    matrix.populateMatrix();
+
+    System.out.println("Populated matrix:");
+    matrix.printMatrix();
+
+    System.out.println("\nFlipped matrix:");
+    matrix.flipMatrix();
+    matrix.printMatrix();
+
+    scanner.close();
   }
 }
